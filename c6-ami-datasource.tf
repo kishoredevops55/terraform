312427991347a64d040b4f1348cdb50f35201b0d
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2022-01-04 19:12:23 +0530

    Update c6-ami-datasource.tf

--- c6-ami-datasource.tf
+++ c6-ami-datasource.tf
@@ -4,7 +4,7 @@ data "aws_ami" "amzlinux" {
   owners = [ "amazon" ]
   filter {
     name = "name"
-    values = [ "amzn2-ami-hvm-*-gp2" ]
+    values = [ "amzn2-ami-kernel-*-gp2" ]
   }
   filter {
     name = "root-device-type"
