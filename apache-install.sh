85df261ae24c211c84e3aa0280c0ac40f09079c6
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2022-01-17 13:53:59 +0530

    Update apache-install.sh

--- apache-install.sh
+++ apache-install.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start  
-echo "<h1>Kishore Sharma Sundaram! AWS Infra created using Terraform in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Kishore Sharma Sundaram! AWS Infra created using Terraform in us-east-1 Region for kishore aws account </h1>" | sudo tee /var/www/html/index.html
