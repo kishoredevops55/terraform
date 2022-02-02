ef59e023a0ec2539f1e11b9a77c616cd29cf1680
kishoredevops55 <96948636+kishoredevops55@users.noreply.github.com>
2022-02-02 16:56:15 +0530

    Update c4-ec2-instance.tf

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.ec2_instance_type
-  count = 1
+  count = var.ec2_instance_count
   key_name      = "devops"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
